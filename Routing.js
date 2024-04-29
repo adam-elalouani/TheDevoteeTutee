959df9a3fe835aae7acce6ccaff9aa41c5858ca4
adam-elalouani <adam_elalouani@horacemann.org>
2024-04-29 13:09:24 -0400

    adam

--- Routing.js
+++ Routing.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const calendarAPI = require('./api.js');
+const calendarAPI = require('./calenderapi.js');
 
 router.post('/scheduleEvent', (req, res) => {
     const { eventName, eventDate } = req.body;
--- server.js
+++ server.js
@@ -4,10 +4,10 @@ const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
 
-// Mount routes
 app.use('/auth', require('./authRoutes'));
 app.use('/events', require('./eventRoutes'));
 app.use('/media', require('./mediaRoutes'));
+app.use('/search', require('./search'))
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
